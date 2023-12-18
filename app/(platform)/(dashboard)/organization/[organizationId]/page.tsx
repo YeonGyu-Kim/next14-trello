61f8fd35eba4c605d0c9b38b6ca715f2e9a3f533
ygkim <ygkim527@naver.com>
2023-12-18 23:41:02 +0900

    submit 시 borad 추가 작업

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,17 @@
+import { createBorad } from '@/actions/create-borad';
+
 export default function OrganizationPage() {
-  return <div>OrganizationPage</div>;
+  return (
+    <div>
+      <form action={createBorad}>
+        <input
+          className='border-black border p-1'
+          id='title'
+          name='title'
+          required
+          placeholder='Enter a board title'
+        />
+      </form>
+    </div>
+  );
 }
