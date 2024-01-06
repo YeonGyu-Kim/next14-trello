f9203751546680659e36e390e92f78a1f8f1edd4
ygkim <ygkim527@naver.com>
2024-01-06 23:47:12 +0900

    Form 컴포넌트 작업

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -1,12 +1,16 @@
 import { cn } from '@/lib/utils';
-import { forwardRef } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { useFormStatus } from 'react-dom';
+import FormErrors from './form-errors';
 
 type FormInputProps = {
   id: string;
   label?: string;
   type?: string;
   placeholder?: string;
-  required?: string;
+  required?: boolean;
   disabled?: boolean;
   errors?: Record<string, string[] | undefined>;
   className?: string;
@@ -14,52 +18,51 @@ type FormInputProps = {
   onBlur?: () => void;
 };
 
-export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
-  (
-    {
-      id,
-      label,
-      type,
-      placeholder,
-      required,
-      disabled,
-      errors,
-      className,
-      defaultValue = '',
-      onBlur,
-    },
-    ref
-  ) => {
-    const { pending } = useFormStatus();
+export default function FormInput(
+  {
+    id,
+    label,
+    type,
+    placeholder,
+    required,
+    disabled,
+    errors,
+    className,
+    defaultValue = '',
+    onBlur,
+  }: FormInputProps,
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  const { pending } = useFormStatus();
 
-    return (
-      <div className='space-y-2'>
-        <div className='space-y-1'>
-          {label ? (
-            <Label
-              htmlFor={id}
-              className='text-xs font-semibold text-neutral-700'
-            >
-              {label}
-            </Label>
-          ) : null}
-          <Input
-            onBlur={onBlur}
-            defaultValue={defaultValue}
-            ref={ref}
-            required={required}
-            name={id}
-            id={id}
-            placeholder={placeholder}
-            type={type}
-            disabled={pending || disabled}
-            className={cn('text-sm px-2 py-1 h-7', className)}
-            aria-describedby={`${id}-error`}
-          />
-        </div>
+  return (
+    <div className='space-y-2'>
+      <div className='space-y-1'>
+        {label ? (
+          <Label
+            htmlFor={id}
+            className='text-xs font-semibold text-neutral-700'
+          >
+            {label}
+          </Label>
+        ) : null}
+        <Input
+          onBlur={onBlur}
+          defaultValue={defaultValue}
+          ref={ref}
+          required={required}
+          name={id}
+          id={id}
+          placeholder={placeholder}
+          type={type}
+          disabled={pending || disabled}
+          className={cn('text-sm px-2 py-1 h-7', className)}
+          aria-describedby={`${id}-error`}
+        />
       </div>
-    );
-  }
-);
+      <FormErrors id={id} errors={errors} />
+    </div>
+  );
+}
 
 FormInput.displayName = 'FormInput';
